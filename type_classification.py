6d2f66a3047911354d7596d2d8d1e6f9d319e9da
Liang-Chun Tsai <lt2590@columbia.edu>
2016-04-09 00:21:02 -0400

    Fix issue

--- type_classification.py
+++ type_classification.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine
 import pandas as pd
 import scipy.io as sio
 import numpy as np
-<<<<<<< HEAD
-<<<<<<< HEAD
 import sys
 import os
 os.environ['GLOG_minloglevel'] = '3' 
@@ -11,17 +9,12 @@ import caffe
 
 def getDist(target, query):
 	return np.linalg.norm(query - target)
-=======
->>>>>>> c378eb0b2dc9114badd0ff5701b553d045215556
-=======
->>>>>>> c378eb0b2dc9114badd0ff5701b553d045215556
 
 if __name__ == '__main__':
 
 	engine = create_engine('sqlite:///amazon/test.db')
 	d = pd.read_sql_table('Amazon', engine)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	#sample_d = d[d["type"].isin(["T-Shirt", "Wool Jacket", "Buttom-Down Shirt", "Dress"])]
 	#hogs = np.array([sio.loadmat("./hog/hog10.mat")['hog'][0] for id in sample_d["id"]])
 
@@ -100,25 +93,3 @@ if __name__ == '__main__':
 	np.save(open("dist.npy", 'wb'), dist)
 	id = dist.argsort()[:10]
 	print id
-
-=======
-=======
->>>>>>> c378eb0b2dc9114badd0ff5701b553d045215556
-
-	h_form = sio.loadmat('hog1.mat')['hog']
-	hogs = np.empty([0, h_form.shape[1]])
-
-	for id in d["id"]:
-		hog = sio.loadmat("hog1.mat")['hog']
-		print id
-		hogs = np.append(hogs, hog, axis = 0)
-		print id, "nn"
-		#hogs = np.array(hogs)
-
-	#hogs = np.array([sio.loadmat("hog1.mat")['hog'] for id in d["id"]])
-	#data = np.array(data)
-<<<<<<< HEAD
->>>>>>> c378eb0b2dc9114badd0ff5701b553d045215556
-=======
->>>>>>> c378eb0b2dc9114badd0ff5701b553d045215556
-
